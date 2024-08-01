ce6fbe2b9f78ce1ee0d341e06acba48a02019964
nong phloeut <phloeutnong@.com>
2024-08-01 14:42:57 +0700

    update ci cd

--- server.js
+++ server.js
@@ -12,5 +12,5 @@ app.get('/', (req, res) => {
 })
 
 app.listen(8080, () => {
-    console.log('server running on port 8080 Hello test')
+    console.log('server running on port 8080 Hello test ci cd')
 })
